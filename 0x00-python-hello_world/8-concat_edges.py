50396c81518a25fcab18bd21903cd648a0332a69
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-09-20 00:51:12 +0300

    Printing specific text in a string

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interractive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[40:67] + str[107:113] + str[0:6]
+print(str)
