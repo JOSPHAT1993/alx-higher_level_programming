ceaf48247b045b442b2098156845d1d31528c9a2
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-09-20 19:12:27 +0300

    printing alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113:
+for char in range(97, 123):
+    if char == 101 or char == 113:
         continue
     else:
-        print(chr(i), end="")
+        print("{:c}".format(char), end="")
