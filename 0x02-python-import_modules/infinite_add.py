8b1355c92a80386e0d894b4baca2f27c6a9a315b
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-09-22 23:17:16 +0300

    printing sum of commandline arguments in python

--- 0x02-python-import_modules/infinite_add.py
+++ 0x02-python-import_modules/infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-def args_add(num_args, args):
-    result = 0
-
-    if num_args > 1:
-        for i in range(1, num_args):
-            result += int(args[i])
-    return result
-
 if __name__ == "__main__":
     from sys import argv
-    print("{:d}".format(args_add(len(argv), argv)))
+
+    args = len(argv)
+    add = 0
+    if args > 1:
+        for i in range(1, args):
+            add += int(argv[i])
+        print(add)
+    else:
+        print(add)
