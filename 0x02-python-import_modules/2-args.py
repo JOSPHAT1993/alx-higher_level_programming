7fe30ed18a01bec28799828e318e56b7f3586d68
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-09-22 23:58:51 +0300

    Printing commaindline arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 if __name__ == '__main__':
     from sys import argv
 
-num_args = len(argv)
-if num_args == 1:
-    print("{:d} arguments.".format(num_args - 1))
-elif num_args == 2:
-    print("{:d} argument:".format(num_args - 1))
-    print("{:d} {}".format(num_args - 1, argv[num_args - 1]))
-else:
-    print("{:d} arguments:".format(num_args - 1))
-    for i in range(1, num_args):
-        print("{:d} : {}".format(i, argv[i]))
+    num_args = len(argv)
+    if num_args == 1:
+        print("{:d} arguments.".format(num_args - 1))
+    elif num_args == 2:
+        print("{:d} argument:".format(num_args - 1))
+        print("{:d} {}".format(num_args - 1, argv[num_args - 1]))
+    else:
+        print("{:d} arguments:".format(num_args - 1))
+        for i in range(1, num_args):
+            print("{:d} : {}".format(i, argv[i]))
