9c9e8d1e2ff42cb179e31e5f0c555341675b9301
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-09-26 17:07:32 +0300

    Max Value of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    largest = my_list[0]
+    largest = -99999
     if my_list == []:
-        return None
+        largest = None
     else:
         for i in range(len(my_list)):
             if my_list[i] > largest:
                  largest = my_list[i]
-        return largest
+    return largest
