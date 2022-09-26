922919fa301790819b492a2d58f519ddf9c7fd09
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-09-26 16:11:37 +0300

    Printing legth and first character of a tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    count = 0
+    if sentence == ():
+        print("None")
+    else:
+        for i in sentence:
+            first = sentence[0]
+            count += 1
+        return count, first
