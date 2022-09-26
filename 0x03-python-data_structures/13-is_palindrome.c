eaa22e39dd329e3648fa4eb6901f7ff8b55e6291
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-09-26 22:25:39 +0300

    Program to check whether a linked list is a palidrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,35 @@
 #include "lists.h"
 /**
- * palidrome-main function to check for palidrome
+ * palindrome-main function to check for palidrome
  *
  * @left:'head of singly list'
  * @right:'tail of a singly list'
  *
  * Return:0 if not palidrom, 1 if it is
  */
-int palidrome(listint_t **left, listint_t right)
+int palindrome(listint_t **left, listint_t right)
 {
-	int is_palidrome;
+	int isPalidrome;
 
 	if (right == NULL)
 	{
 		return (1);
 	}
 
-	is_palidrome = (palidrome(left, right->next) && (*left)->n == right->n);
+	isPalidrome = (palidrome(left, right->next) && (*left)->n == right->n);
 	*left = (*left)->next;
 
-	return (is_palidrome);
+	return (isPalidrome);
 }
 
 /**
- * is_paledrome-function to check for palidrome
+ * is_palindrome-function to check for palidrome
  *
  * @head:'singly list head'
  *
  * Return:0 if not palidrom, 1 if palidrome
  */
-int is_paledrome(listint_t **head)
+int is_palindrome(listint_t **head)
 {
 	if (!(*head) || !head)
 		return (1);
