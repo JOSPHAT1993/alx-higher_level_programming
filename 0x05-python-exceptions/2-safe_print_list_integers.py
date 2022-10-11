724aff76af60cb5c464f98f09e81e1dcac03da8b
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-11 22:07:35 +0300

    Printing integer values using try exception but with an error

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    p = 0
+
+    if (my_list):
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                p += 1
+            except (ValueError, TypeError):
+                pass
+    print()
+    return (p)
