a95de831e56da2e0e67f8ef37bb060a2c2de70ab
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-11 22:59:12 +0300

    Printing division of two lists and handling exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,14 +4,15 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         result = 0
+
         try:
             result = my_list_1[i] / my_list_2[i]
-        except (ZeroDivisionError):
-            print("{}".format("division by 0"))
+        except ZeroDivisionError:
+            print("division by 0")
         except (ValueError, TypeError):
-            print("{}".format("wrong type"))
+            print("wrong type")
         except IndexError:
-            print("{}".format("out of range"))
+            print("out of range")
         finally:
             newList.append(result or 0)
     return newList
