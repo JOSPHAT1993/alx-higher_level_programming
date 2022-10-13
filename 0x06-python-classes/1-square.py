f7623a740f43025b6ea3fcbccbab67d83aeae3a8
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-13 17:20:07 +0300

    Creating a class and with a private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """
+    1-square.py
+
+
+    defining class with size
+    """
+    def __init__(self, size):
+        """__init__ Contructor
+
+        Contructor of class square
+
+        Args:
+            size(int): private class attribute
+        """
+        self.__size = size
