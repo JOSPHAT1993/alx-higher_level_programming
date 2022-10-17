914d0ee95952761f9c0d12b799f7fb75c190e0e8
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-17 14:06:13 +0300

    Creating a class Rectangle and instance of width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,19 +10,19 @@ class Rectangle:
         self.height = height
         self.width = width
 
-    """def width(self):
+    def width(self):
         return self.__width
-    """
+
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
-    """def height(self):
+
+    def height(self):
         return self.__height
-    """
+
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
