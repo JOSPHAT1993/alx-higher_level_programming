80d9eaccbc2835d7463c474f0e7e6e266e9df018
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-17 15:49:44 +0300

    Creating a class with width attributes and height private attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,19 +6,12 @@
         define attribute height.
 """
 class Rectangle:
-    def __init__(self, height, width):
+    """Rectanle class
+
+    """
+    def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-
-    def width(self):
-        return self.__width
-
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     def height(self):
         return self.__height
@@ -29,3 +22,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
