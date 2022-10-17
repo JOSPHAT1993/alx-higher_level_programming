ac4a09ad8bfb62f37e9d63ebed5424b41d6f0791
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-17 16:28:43 +0300

    Creating class, attributes of class width and height and using getters and setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,31 +2,42 @@
 """1-rectangle
 
     Todo:
-        define attribute width.
-        define attribute height.
+        define attribute width setter and getter.
+        define attribute height setter and getter.
 """
+
+
 class Rectangle:
     """Rectanle class
 
     """
     def __init__(self, width=0, height=0):
+        """Constructor"""
         self.height = height
         self.width = width
 
+    @property
     def height(self):
+        """height getter"""
         return self.__height
 
+    @height.setter
     def height(self, value):
+        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    @property
     def width(self):
+        """width getter"""
         return self.__width
 
+    @width.setter
     def width(self, value):
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
