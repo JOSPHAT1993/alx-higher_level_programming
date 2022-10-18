a66c9398df86045eafa5623bfe7b5b2ce50a2afb
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-18 15:54:07 +0300

    Locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" 101-locked_class
+    LockedClass
+"""
 
 
-class LockedClass:
+class LockedClass():
+    """LockedClass
+    attribute "first_name"
+    """
     __slots__ = ["first_name"]
