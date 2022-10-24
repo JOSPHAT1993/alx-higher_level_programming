daf07b755468f4457322d3a916bd1dd73d5f933d
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-24 15:41:47 +0300

    Creating empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""5-base_geometry
+
+Todo:
+    Empty class
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def __init__(self):
+        """class constructor"""
+        pass
