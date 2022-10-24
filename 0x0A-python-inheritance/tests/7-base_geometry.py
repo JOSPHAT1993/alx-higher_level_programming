a704150b803fdf87b3b7064ec227c69eb5caa17c
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-24 16:15:26 +0300

    Creating integer validator in BaseGeometry class

--- 0x0A-python-inheritance/tests/7-base_geometry.py
+++ 0x0A-python-inheritance/tests/7-base_geometry.py
@@ -28,10 +28,7 @@ class BaseGeometry:
         name: str
         value: int
         """
-        if isinstance(name, str):
-            pass
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
