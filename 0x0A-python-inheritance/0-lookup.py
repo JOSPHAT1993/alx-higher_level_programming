054d2cec334a3f80125c07d6f378da3a42879365
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-24 12:39:56 +0300

    lookup method using dir

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""0-lookup
+
+Todo: prints list of attr and methods of obj
+"""
+
+
+def lookup(obj):
+    """lookup class
+
+    Args:
+        obj (any): object
+
+    Return:
+        lists
+    """
+    return dir(obj)
