a1c926418c32a01567a03fc15d376b01df649b28
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-24 15:49:09 +0300

    creating area method in a class'

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""6-base_geometry
+
+Todo:
+    Creates an Empty Class
+    Create method area which return exeption
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def __init__(self):
+        """class constructor"""
+        pass
+
+    def area(self):
+        """area method
+
+        Raises exception
+        """
+        raise Exception("area() is not implemented")
