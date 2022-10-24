584c60a8035edae7991f0da960f90d675102e0d7
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-24 16:12:14 +0300

    Creating integer validator in BaseGeometry class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""7-base_goemetry
+
+Todo:
+    Creates a class BaseGeometry
+    Within the class create empty method area with exception
+    Within the class create a methode integer_validator
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def __init__(self):
+        """class constructor"""
+        pass
+
+    def area(self):
+        """area method
+
+        Returns: Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """method validating for name and value
+
+        name: str
+        value: int
+        """
+        if isinstance(name, str):
+            pass
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
