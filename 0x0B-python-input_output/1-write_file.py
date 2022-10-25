eedb4e9306827090b3477c4bdbd114672caccf1a
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-25 12:44:42 -0700

    writting to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,8 @@ Todo:
     Write to a file.
 """
 
+
 def write_file(filename="", text=""):
+    """writes contents to a file"""
     with open(filename, mode='w', encoding="UTF-8") as f:
         return f.write(text)
