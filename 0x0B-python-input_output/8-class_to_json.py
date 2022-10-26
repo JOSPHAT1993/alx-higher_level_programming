2adf501dcdae01e930e03a4f945b957563a2ea74
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-10-26 23:14:09 +0300

    Printing the class descrition

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""8-class_to_json
+
+Todo:
+    To returns simple description of class object
+"""
+
+
+def class_to_json(obj):
+    """class_to_json description"""
+    return (obj.__dict__)
