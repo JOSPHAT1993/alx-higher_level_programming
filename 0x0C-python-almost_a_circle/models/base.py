f453674648d062e22049cf75df2e60943eb6d1d5
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-11-02 06:51:36 +0300

    Class base task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+"""Base Class
+
+Todo:
+    define a private class attribute nbobjects
+    initialize the class with init method
+"""
+
+
+class Base:
+    """class Base"""
+    _nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor"""
+        if id not None:
+            self.id = id
+        _nb_objects += 1
+        self.id = _nb_objects
+
 
