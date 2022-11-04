51a0b4cb2f538260e9727546d0d50dc083ea84a4
JOSPHAT1993 <josphatmutuakituku@gmail.com>
2022-11-04 12:35:19 +0300

    Class Square inheriting all variables in Rectangle class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""Square class
 
+Todo:
+    creates the class square.
+    calls the super class Rectangle.
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """class constructor"""
+        super().__init__(size, size, x, y, id)
